c48e00d0957b6c2fac4785001d991d3599d2dada
Ruan Bruno <81209795+siann-web@users.noreply.github.com>
2024-04-11 23:20:28 -0300

    Add files via upload

---
+++ Trabalho Tabuada Rangel/script.js
@@ -0,0 +1,252 @@
+const numero_1 = document.getElementById('um').addEventListener('click', function(){
+    let numero = 1;
+    let tabuada = '';
+    let add_evento_operaçao = document.getElementById('add').addEventListener('click', function(){
+        if(tabuada != ''){tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                tabuada += `${numero} + ${i} = ${numero + i}<br>`;
+                resultado.innerHTML = tabuada;
+            }});
+     let sub_evento_operaçao = document.getElementById('substract').addEventListener('click', function(){
+        if(tabuada != ''){tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                tabuada += `${numero} - ${i} = ${numero - i}<br>`;
+                resultado.innerHTML = tabuada;
+            }});
+     let mult_evento_operaçao = document.getElementById('multiply').addEventListener('click', function(){
+        if(tabuada != ''){tabuada = '';} 
+            for(let i = 1; i <= 10; i++){
+                tabuada += `${numero} x ${i} = ${numero * i}<br>`;
+                resultado.innerHTML = tabuada;
+            }});
+     let div_evento_operaçao = document.getElementById('divide').addEventListener('click', function(){
+        if(tabuada != ''){tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                tabuada += `${numero} / ${i} = ${numero / i}<br>`;
+                resultado.innerHTML = tabuada;
+            }});            
+});
+const numero_2 = document.getElementById('dois').addEventListener('click', function(){
+    let numero = 2;
+    let tabuada = '';
+    let add_evento_operaçao = document.getElementById('add').addEventListener('click', function(){
+        if(tabuada != ''){tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                tabuada += `${numero} + ${i} = ${numero + i}<br>`;
+                resultado.innerHTML = tabuada;
+        }});
+     let sub_evento_operaçao = document.getElementById('substract').addEventListener('click', function(){
+        if(tabuada != ''){tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                tabuada += `${numero} - ${i} = ${numero - i}<br>`;
+                resultado.innerHTML = tabuada;
+            }});
+     let mult_evento_operaçao = document.getElementById('multiply').addEventListener('click', function(){
+        if(tabuada != ''){tabuada = '';} 
+            for(let i = 1; i <= 10; i++){
+                tabuada += `${numero} x ${i} = ${numero * i}<br>`;
+                resultado.innerHTML = tabuada;
+            }});
+     let div_evento_operaçao = document.getElementById('divide').addEventListener('click', function(){
+        if(tabuada != ''){tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                tabuada += `${numero} / ${i} = ${numero / i}<br>`;
+                resultado.innerHTML = tabuada;
+            }});            
+});
+const numero_3 = document.getElementById('tres').addEventListener('click', function(){
+    let numero = 3;
+    let tabuada = '';
+    let add_evento_operaçao = document.getElementById('add').addEventListener('click', function(){
+        if(tabuada != ''){tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                tabuada += `${numero} + ${i} = ${numero + i}<br>`;
+                resultado.innerHTML = tabuada;
+        }});
+     let sub_evento_operaçao = document.getElementById('substract').addEventListener('click', function(){
+        if(tabuada != ''){tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                tabuada += `${numero} - ${i} = ${numero - i}<br>`;
+                resultado.innerHTML = tabuada;
+            }});
+     let mult_evento_operaçao = document.getElementById('multiply').addEventListener('click', function(){
+        if(tabuada != ''){tabuada = '';} 
+            for(let i = 1; i <= 10; i++){
+                tabuada += `${numero} x ${i} = ${numero * i}<br>`;
+                resultado.innerHTML = tabuada;
+            }});
+     let div_evento_operaçao = document.getElementById('divide').addEventListener('click', function(){
+        if(tabuada != ''){tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                tabuada += `${numero} / ${i} = ${numero / i}<br>`;
+                resultado.innerHTML = tabuada;
+            }});            
+});
+const numero_4 = document.getElementById('quatro').addEventListener('click', function(){
+    let numero = 4;
+    let tabuada = '';
+    let add_evento_operaçao = document.getElementById('add').addEventListener('click', function(){
+        if(tabuada != ''){tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                tabuada += `${numero} + ${i} = ${numero + i}<br>`;
+                resultado.innerHTML = tabuada;
+        }});
+     let sub_evento_operaçao = document.getElementById('substract').addEventListener('click', function(){
+        if(tabuada != ''){tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                tabuada += `${numero} - ${i} = ${numero - i}<br>`;
+                resultado.innerHTML = tabuada;
+            }});
+     let mult_evento_operaçao = document.getElementById('multiply').addEventListener('click', function(){
+        if(tabuada != ''){tabuada = '';} 
+            for(let i = 1; i <= 10; i++){
+                tabuada += `${numero} x ${i} = ${numero * i}<br>`;
+                resultado.innerHTML = tabuada;
+            }});
+     let div_evento_operaçao = document.getElementById('divide').addEventListener('click', function(){
+        if(tabuada != ''){tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                tabuada += `${numero} / ${i} = ${numero / i}<br>`;
+                resultado.innerHTML = tabuada;
+            }});            
+});
+const numero_5 = document.getElementById('cinco').addEventListener('click', function(){
+    let numero = 5;
+    let tabuada = '';
+    let add_evento_operaçao = document.getElementById('add').addEventListener('click', function(){
+        if(tabuada != ''){tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                tabuada += `${numero} + ${i} = ${numero + i}<br>`;
+                resultado.innerHTML = tabuada;
+        }});
+     let sub_evento_operaçao = document.getElementById('substract').addEventListener('click', function(){
+        if(tabuada != ''){tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                tabuada += `${numero} - ${i} = ${numero - i}<br>`;
+                resultado.innerHTML = tabuada;
+            }});
+     let mult_evento_operaçao = document.getElementById('multiply').addEventListener('click', function(){
+        if(tabuada != ''){tabuada = '';} 
+            for(let i = 1; i <= 10; i++){
+                tabuada += `${numero} x ${i} = ${numero * i}<br>`;
+                resultado.innerHTML = tabuada;
+            }});
+     let div_evento_operaçao = document.getElementById('divide').addEventListener('click', function(){
+        if(tabuada != ''){tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                tabuada += `${numero} / ${i} = ${numero / i}<br>`;
+                resultado.innerHTML = tabuada;
+            }});            
+});
+const numero_6 = document.getElementById('seis').addEventListener('click', function(){
+    let numero = 6;
+    let tabuada = '';
+    let add_evento_operaçao = document.getElementById('add').addEventListener('click', function(){
+        if(tabuada != ''){tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                tabuada += `${numero} + ${i} = ${numero + i}<br>`;
+                resultado.innerHTML = tabuada;
+        }});
+     let sub_evento_operaçao = document.getElementById('substract').addEventListener('click', function(){
+        if(tabuada != ''){tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                tabuada += `${numero} - ${i} = ${numero - i}<br>`;
+                resultado.innerHTML = tabuada;
+            }});
+     let mult_evento_operaçao = document.getElementById('multiply').addEventListener('click', function(){
+        if(tabuada != ''){tabuada = '';} 
+            for(let i = 1; i <= 10; i++){
+                tabuada += `${numero} x ${i} = ${numero * i}<br>`;
+                resultado.innerHTML = tabuada;
+            }});
+     let div_evento_operaçao = document.getElementById('divide').addEventListener('click', function(){
+        if(tabuada != ''){tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                tabuada += `${numero} / ${i} = ${numero / i}<br>`;
+                resultado.innerHTML = tabuada;
+            }});            
+});
+const numero_7 = document.getElementById('sete').addEventListener('click', function(){
+    let numero = 7;
+    let tabuada = '';
+    let add_evento_operaçao = document.getElementById('add').addEventListener('click', function(){
+        if(tabuada != ''){tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                tabuada += `${numero} + ${i} = ${numero + i}<br>`;
+                resultado.innerHTML = tabuada;
+        }});
+     let sub_evento_operaçao = document.getElementById('substract').addEventListener('click', function(){
+        if(tabuada != ''){tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                tabuada += `${numero} - ${i} = ${numero - i}<br>`;
+                resultado.innerHTML = tabuada;
+            }});
+     let mult_evento_operaçao = document.getElementById('multiply').addEventListener('click', function(){
+        if(tabuada != ''){tabuada = '';} 
+            for(let i = 1; i <= 10; i++){
+                tabuada += `${numero} x ${i} = ${numero * i}<br>`;
+                resultado.innerHTML = tabuada;
+            }});
+     let div_evento_operaçao = document.getElementById('divide').addEventListener('click', function(){
+        if(tabuada != ''){tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                tabuada += `${numero} / ${i} = ${numero / i}<br>`;
+                resultado.innerHTML = tabuada;
+            }});            
+});
+const numero_8 = document.getElementById('oito').addEventListener('click', function(){
+    let numero = 8;
+    let tabuada = '';
+    let add_evento_operaçao = document.getElementById('add').addEventListener('click', function(){
+        if(tabuada != ''){tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                tabuada += `${numero} + ${i} = ${numero + i}<br>`;
+                resultado.innerHTML = tabuada;
+        }});
+     let sub_evento_operaçao = document.getElementById('substract').addEventListener('click', function(){
+        if(tabuada != ''){tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                tabuada += `${numero} - ${i} = ${numero - i}<br>`;
+                resultado.innerHTML = tabuada;
+            }});
+     let mult_evento_operaçao = document.getElementById('multiply').addEventListener('click', function(){
+        if(tabuada != ''){tabuada = '';} 
+            for(let i = 1; i <= 10; i++){
+                tabuada += `${numero} x ${i} = ${numero * i}<br>`;
+                resultado.innerHTML = tabuada;
+            }});
+     let div_evento_operaçao = document.getElementById('divide').addEventListener('click', function(){
+        if(tabuada != ''){tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                tabuada += `${numero} / ${i} = ${numero / i}<br>`;
+                resultado.innerHTML = tabuada;
+            }});            
+});
+const numero_9 = document.getElementById('nove').addEventListener('click', function(){
+    let numero = 9;
+    let tabuada = '';
+    let add_evento_operaçao = document.getElementById('add').addEventListener('click', function(){
+        if(tabuada != ''){tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                tabuada += `${numero} + ${i} = ${numero + i}<br>`;
+                resultado.innerHTML = tabuada;
+        }});
+     let sub_evento_operaçao = document.getElementById('substract').addEventListener('click', function(){
+        if(tabuada != ''){tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                tabuada += `${numero} - ${i} = ${numero - i}<br>`;
+                resultado.innerHTML = tabuada;
+            }});
+     let mult_evento_operaçao = document.getElementById('multiply').addEventListener('click', function(){
+        if(tabuada != ''){tabuada = '';} 
+            for(let i = 1; i <= 10; i++){
+                tabuada += `${numero} x ${i} = ${numero * i}<br>`;
+                resultado.innerHTML = tabuada;
+            }});
+     let div_evento_operaçao = document.getElementById('divide').addEventListener('click', function(){
+        if(tabuada != ''){tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                tabuada += `${numero} / ${i} = ${numero / i}<br>`;
+                resultado.innerHTML = tabuada;
+            }});            
+});
